23f066a1888edc2cec0793e07af78d8a331685d2
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-26 15:03:00 +0000

    Render multiple components on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ const Home = () => (
 		<h1>Home</h1>
 		<p>This is a test of the homepage.</p>
 
-		<Button />
+		<Button>£6</Button>
+		<Button>£12</Button>
+		<Button>£18</Button>
+		<Button>£30</Button>
 	</>
 )
 
