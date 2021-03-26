ffbb3060d574701920ec9bc22b4c3cd31905518b
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-26 14:28:34 +0000

    Import the Button component to render on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from '../components/atoms/button/Button'
 
 const Home = () => (
 	<>
 		<h1>Home</h1>
 		<p>This is a test of the homepage.</p>
+
+		<Button />
 	</>
 )
 
