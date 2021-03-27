6dcdabcf313177b2c4476b19804b34519199db05
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-27 21:24:57 +0000

    Donations tab component built

--- src/components/molecules/donation/Donation.js
+++ src/components/molecules/donation/Donation.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../atoms/button/Button'
 
 const Donation = () => {
+
+	const [ toggleState, setToggleState ] = useState(1)
+
+	const toggleTab = (index) => {
+		setToggleState(index)
+	} 
 	
 	return (
-		<div>Donations</div>
+		<div className='container'>
+			<div className='bloc-tabs'>
+				<button className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Donate monthly</button>
+				<button className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Donate once</button>
+			</div>
+			<div className={toggleState === 1 ? "content  active-content" : "content"}>
+				<p>I would like to make a monthly donation of</p>
+				<Button>£12</Button>
+			</div>
+
+			<div className={toggleState === 2 ? "content  active-content" : "content"}>
+				<p>I would like to make a one-off donation of</p>
+				<Button>£12</Button>
+			</div>
+		</div>
 	)
 }
 
