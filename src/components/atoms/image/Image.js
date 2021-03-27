f979b19c1600f521be28c52c3eb6793390039438
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-27 11:31:47 +0000

    Image component added

---
+++ src/components/atoms/image/Image.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Image = () => {
+	
+	return (
+		<img src='image.jpg' alt='' />
+	)
+}
+
+export default Image
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Button from '../components/atoms/button/Button'
 import Hyperlink from '../components/atoms/hyperlink/Hyperlink'
+import Image from '../components/atoms/image/Image'
 import Donation from '../components/molecules/donation/Donation'
 
 const Home = () => (
 	<>
 		<h1>Home</h1>
 		<p>This is a test of the homepage.</p>
+
+		<Image />
 
 		<Button className='test-btn'>£6</Button>
 		<Button>£12</Button>
