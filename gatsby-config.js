db919c44e8196160b195ff1a62d5782a90ab8b6a
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-29 00:25:31 +0100

    Gatsby image plugins installed and added to the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	plugins: [
-		`gatsby-plugin-sass`
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp` // Needed for dynamic images
 	]
 }
